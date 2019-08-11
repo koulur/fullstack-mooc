865564ae4734bc72c5e1333219a0837f8267b40b
Rami <ramilejmi@gmail.com>
2019-08-12 00:02:55 +0300

    2.14*

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 
-const Numbers = ({ persons, newFilter, setPersons }) => {
-  const filtered = persons.filter((person) => person.name.includes(newFilter))
+const Numbers = ({ persons, newFilter }) => {
+  console.log('persons from number: ', persons)
+  const filtered = persons.filter((person) => person !== undefined).filter((person) => person.name.includes(newFilter))
   return (
     <div>
       <h2>Numbers</h2>
@@ -51,17 +52,20 @@ const Form = (props) => {
   )
 }
 
-const App = () => {
 
+
+const App = () => {
   const [persons, setPersons] = useState([])
+  const [newPerson, setNewPerson] = useState()
 
   useEffect(() => {
+    console.log('persons from useEffect: ', persons)
     axios
         .get('http://localhost:3001/persons')
         .then(response => {
           setPersons(response.data)
         })
-  }, [])
+  }, [newPerson])
 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
@@ -87,9 +91,18 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
       return
     }
-    setPersons(persons.concat({name: newName, number: newNumber}))
-    setNewName('')
-    setNewNumber('')
+    setNewPerson({name: newName, number: newNumber})
+    setPersons(persons.concat(newPerson))
+    
+
+    axios.post('http://localhost:3001/persons', 
+              {name: newName, number: newNumber})
+          .then(() => {
+            setNewName('')
+            setNewNumber('')
+            }
+          )
+
   }
 
   
